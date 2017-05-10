d61b36397385d9a8187a64594cbe1ac6499c2f9a
Shekhar Sahu <chandrashekhar@joshsoftware.com>
2017-05-10 12:08:17 +0530

    Release xrbia service build version 0.2

--- app/src/main/java/com/simplysmart/service/config/ApplicationConstant.java
+++ app/src/main/java/com/simplysmart/service/config/ApplicationConstant.java
@@ -3,13 +3,13 @@ package com.simplysmart.service.config;
 public class ApplicationConstant {
 
     //Flag for debugging log mode
-    public static boolean isDebuggable = true;
+    public static boolean isDebuggable = false;
 
     //Api url component
     public static final String PROTOCOL = "http://";
-        public static final String DOMAIN ="192.168.1.59:3000";
+//        public static final String DOMAIN ="192.168.1.59:3000";
 //    public static final String DOMAIN ="api.simplysmart.tech";
-//    public static final String DOMAIN = "township.xrbia.com";
+    public static final String DOMAIN = "township.xrbia.com";
 
     public static final String SERVER_URL = PROTOCOL + DOMAIN;
 
